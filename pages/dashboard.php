d500000003a6f33d7cb7318847a5605ed1fb269b
Anthony Macchia <macchiaa8@students.rowan.edu>
2019-04-23 18:24:35 -0400

    Show dialog box when create new week button is clicked

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -6,4 +6,29 @@
 	}
 
 ?>
+
+
+
+
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="newWeekModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	</button>
+      </div>
+      <div class="modal-body">
+        <h6> Enter work week info here </h6>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
 </div>
--- pages/header.php
+++ pages/header.php
@@ -6,8 +6,9 @@
         $pg = $_GET["page"];
 
         if ($pg === 'dashboard') {
-            echo '<a href="#" class="btn btn-secondary btn-lg active" role="button">Create New Work Week</a> &nbsp;';
-            echo '<a href="./index1.php?page=login" class="btn btn-secondary btn-lg active" role="button">Sign-Out</a>';
+            //echo '<a href="#"  class="btn btn-secondary btn-lg active" role="button">Create New Work Week</a> &nbsp;';
+	    echo '<button type="button" class="btn btn-secondary btn-lg" data-toggle="modal" data-target="#newWeekModal">Create New Work Week</button> &nbsp;';
+	    echo '<a href="./index1.php?page=login" class="btn btn-secondary btn-lg" role="button">Sign-Out</a>';
         }
     }
   ?>
